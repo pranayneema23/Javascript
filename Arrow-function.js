97fb25b776104e7fbef22a65ef70f7764752709e
Pranay Neema <neema.pranay23@gmail.com>
2020-06-21 22:50:47 +0530

    Create Arrow-function.js
    Code to understand the basics of arrow-function.

---
+++ Arrow-function.js
@@ -0,0 +1,18 @@
+//Nomal function declaration
+/*function printMyName(name) {
+  console.log(name);
+}*/
+
+//Arrow syntex
+/*const printMyName = (name) => {
+  console.log(name);
+}*/
+
+//Shortern the syntex
+const printMyName = name => console.log(name);
+
+//No need of return 
+const doubleNumber = number => number * 2;
+
+printMyName('Pranay');
+console.log(doubleNumber(5));

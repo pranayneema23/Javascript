f24e4dc75b7e9c97606023d0381b632b3a94db81
Pranay Neema <neema.pranay23@gmail.com>
2020-06-23 17:08:06 +0530

    Create Array-Functions.js
    Understand the map function as of now.

---
+++ Array-Functions.js
@@ -0,0 +1,7 @@
+//Array Functions
+//map, reduce, filter, slice, splice
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => num*2);
+console.log(numbers);
+console.log(doubleNumArray);
